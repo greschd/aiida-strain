bbe91fad3dda1da5cf71917cc6bdb2a054eea494
Dominik Gresch <greschd@gmx.ch>
2019-01-24 09:42:11 +0100

    Move install info to setup.json.

--- setup.py
+++ setup.py
@@ -4,42 +4,22 @@
 # Author: Dominik Gresch <greschd@gmx.ch>
 
 import re
+import os
+import json
 from setuptools import setup, find_packages
 
 # Get the version number
 with open('./aiida_strain/__init__.py') as f:
-    match_expr = "__version__[^'\"]+(['\"])([^'\"]+)"
-    version = re.search(match_expr, f.read()).group(2).strip()
+    MATCH_EXPR = "__version__[^'\"]+(['\"])([^'\"]+)"
+    VERSION = re.search(MATCH_EXPR, f.read()).group(2).strip()
+
+SETUP_JSON_PATH = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'setup.json')
+with open(SETUP_JSON_PATH, 'r') as json_file:
+    SETUP_KWARGS = json.load(json_file)
 
 if __name__ == '__main__':
     setup(
-        name='aiida-strain',
-        version=version,
-        description='AiiDA Plugin for applying strain to structures',
-        author='Dominik Gresch',
-        author_email='greschd@gmx.ch',
-        url='https://aiida-strain.readthedocs.io',
-        license='Apache 2.0',
-        classifiers=[
-            'Development Status :: 3 - Alpha', 'Environment :: Plugins',
-            'Intended Audience :: Science/Research',
-            'License :: OSI Approved :: Apache Software License',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3.6',
-            'Topic :: Scientific/Engineering :: Physics'
-        ],
-        keywords='strain aiida workflows',
+        version=VERSION,
         packages=find_packages(exclude=['aiida']),
-        include_package_data=True,
-        setup_requires=['reentry'],
-        reentry_register=True,
-        install_requires=[
-            'aiida-core', 'strain', 'aiida-symmetry-representation',
-            'aiida-tools'
-        ],
-        extras_require={
-            'test': ['aiida-pytest', 'pytest'],
-            'pre-commit': ['pre-commit', 'yapf==0.25']
-        },
-        entry_points={},
-    )
+        **SETUP_KWARGS)
