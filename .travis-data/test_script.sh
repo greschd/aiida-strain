9db4f17260a980625dfc2a8f3409a11a5bf0a920
Dominik Gresch <a-dogres@microsoft.com>
2019-11-22 13:35:01 +0100

    Update Travis setup.

--- .travis-data/test_script.sh
+++ .travis-data/test_script.sh
@@ -11,7 +11,6 @@ case "$TEST_TYPE" in
     tests)
         # Run the AiiDA tests
         python ${TRAVIS_BUILD_DIR}/.travis-data/configure.py ${TRAVIS_BUILD_DIR}/.travis-data/test_config.yml ${TRAVIS_BUILD_DIR}/tests/config.yml;
-        export AIIDA_PATH="${TRAVIS_BUILD_DIR}/tests"
         cd ${TRAVIS_BUILD_DIR}/tests; pytest --quiet-wipe --print-status
         ;;
     pre-commit)
